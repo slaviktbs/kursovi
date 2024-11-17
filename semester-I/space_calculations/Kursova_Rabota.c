d679eb13dda5d24fedb2d4cc35706426fb3f998f
slaviktbs <slaviktbs@gmail.com>
2024-11-17 14:50:52 +0300

    initial commit

---
+++ semester-I/space_calculations/Kursova_Rabota.c
@@ -0,0 +1,34 @@
+/*
+    Програмата пресмята расхода на горивото, време за пътуване... ,
+    като исползва дани въведени от потребител:
+        мястото на старта (cosmodrome)
+        дистинацията (destination)
+        даните за времето във съответната дистинация (взима се от интернет, съхранява се във файла и се използва от програмата)
+        масата на товара (mass)
+        
+*/
+#include <stdio.h>
+
+
+int main(void)
+{
+    int first_choose second_choose;
+    double ;
+    char Moon, Mars;
+    printf ("Choose the destination: Moon (1) or Mars (2) \n");
+    printf ("Click 1 or 2 to choose it \n");
+    scanf("%d", &first_choose);
+    printf ("You have chosen: %d", first_choose);
+
+
+    printf ("Choose the cosmodrome: \n");
+/*  тук трябва да се добави списъка от файла за избор
+    след което да се взме от интернет данните за времето в дадения ден и час и по-нататък да се съобразява с тях,
+    като съпротивление на въздух и тн 
+*/ 
+    printf ("Click from 1 to 2 to choose it \n");
+    scanf("%d", &second_choose);
+    printf ("You have chosen: %d", second_choose);
+
+
+}
