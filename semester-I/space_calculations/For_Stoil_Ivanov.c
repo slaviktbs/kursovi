293496217ca5a25989a71384fbe3696976c1819f
slaviktbs <slaviktbs@gmail.com>
2024-12-20 15:10:34 +0300

    222

--- semester-I/space_calculations/For_Stoil_Ivanov.c
+++ semester-I/space_calculations/For_Stoil_Ivanov.c
@@ -2,28 +2,34 @@
     като сметките се правят в система СИ (SI)
 */
 
-#include "stdio.h"
-#include "math.h"
+#include <stdio.h>
+#include <math.h>
 
+/*
+long double myfunc (long double mu, long double a_par)
+{
+    return sqrtl(mu/a_par);
+}
+*/
 
 int main(void)
 {
-    long unsigned mu, v_par, a_par, s, a_tar;
+    long mu, v_par, a_par, s, a_tar;
 
-    mu = 3986004419*10^5;
+    mu = 398600441900000;
     a_par = 7000000;
     a_tar = 42164000;
 
 /*
     printf ("Enter the radius of the parking orbit");    // Потребителят въвежда a_par
-    scanf ("%f", &a_par);                   
+    scanf ("%ld", &a_par);                   
 
     printf ("Enter the radius of the target orbit");      // Потребителят въвежда a_tar
-    scanf ("%f", &a_tar);
+    scanf ("%ld", &a_tar);
 */
 
-    v_par = sqrt(mu/a_par);
-    printf ("%u \n", v_par);
+    v_par = sqrtl(mu/a_par);
+    printf ("%ld \n", v_par);
  
     return 0;
 }
