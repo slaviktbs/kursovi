845a188740ce60d96ca91c4b80edc830a3751cda
slaviktbs <slaviktbs@gmail.com>
2025-01-21 16:24:48 +0300

    Task 2 - Orbital Inclination Change

---
+++ semester-I/space_calculations/Orbital_Inclination_Change.c
@@ -0,0 +1,10 @@
+/* The program calculates the orbital inclination change (Problem 2 of the third presentation) with calculations made in the SI system.  */
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    
+    return 0;
+}
