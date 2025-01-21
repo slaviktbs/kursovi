679f10fb7f3d9889fd652b1c8487daa4319513ab
slaviktbs <slaviktbs@gmail.com>
2025-01-21 14:21:02 +0300

    add compile instruction

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# kursovi
-kursovi
+## How to compile code
+
+> gcc -o \<name of the output file> \<name of the source file> -lm
+
+
+*compile:*
+
+    gcc -o executable_file executble_file.c -lm
+
+*execute already compiled file:*
+
+    ./executable_file
--- semester-I/space_calculations/For_Stoil_Ivanov.c
+++ semester-I/space_calculations/For_Stoil_Ivanov.c
@@ -2,50 +2,27 @@
     като сметките се правят в система СИ (SI)
 */
 
-/*
+
 #include <stdio.h>
 #include <math.h>
 
 int main(void)
 {
-    float mu, v_par, a_par, s, a_tar;
+    double mu, v_par, a_par, s, a_tar;
 
     mu = 398600441900000;
-    a_par = 7000000;
-    a_tar = 42164000;
-   // v_par = sqr(3);
-
+    // a_par = 7000000;
+    // a_tar = 42164000;
 
     printf ("Enter the radius of the parking orbit");    // Потребителят въвежда a_par
-    scanf ("%ld", &a_par);                   
+    scanf ("%lf", &a_par);                   
 
     printf ("Enter the radius of the target orbit");      // Потребителят въвежда a_tar
-    scanf ("%ld", &a_tar);
-
+    scanf ("%lf", &a_tar);
 
     v_par = sqrt(mu/a_par);
-    printf ("%f", v_par);
+
+    printf ("%lf", v_par);
  
     return 0;
 }
-*/
-
-//---------------------------
-
-// /*
-//#include "stdafx.h"
-
-#include"math.h"
-#include <stdio.h>
-
-int main(void)
-{
-    double v, s;
-
-    s = 67.0/54.0;
-    v = sqrt(s);
-    printf ("%lf", v);
- 
-    return 0;
-}
-// */
