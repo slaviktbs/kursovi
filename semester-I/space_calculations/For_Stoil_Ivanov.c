e89c3def56053177c69d5754bab7a34f5979e009
slaviktbs <slaviktbs@gmail.com>
2025-01-21 15:10:53 +0300

    formuls

--- semester-I/space_calculations/For_Stoil_Ivanov.c
+++ semester-I/space_calculations/For_Stoil_Ivanov.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-    double mu, v_par, a_par, s, a_tar;
+    double mu, v_par, a_par, v_tr_per, v_tr_apo, v_tar, a_tar, a_tr, delta_v, delta_v_2;
 
     mu = 398600441900000;
-    // a_par = 7000000;
+    // a_par = 7000000; 
     // a_tar = 42164000;
 
     printf ("Enter the radius of the parking orbit");    // Потребителят въвежда a_par
@@ -22,7 +22,21 @@ int main(void)
 
     v_par = sqrt(mu/a_par);
 
-    printf ("%lf", v_par);
+    a_tr = (a_par+a_tar)/2;
+
+    v_tr_per = sqrt(mu*(2/a_par-1/a_tr));
+
+    delta_v = v_tr_per - v_par;
+
+    v_tr_apo = sqrt(mu*(2/a_tar-1/a_tr));
+
+    v_tar = sqrt(mu/a_tar);
+
+    delta_v_2 = v_tar - v_tr_apo;
+
+    printf ("/tdelta v1 = %lf/n", delta_v);
+
+    printf ("/tdelta v2 = %lf/n", delta_v_2);
  
     return 0;
 }
