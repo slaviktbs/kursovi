ca5d80a6d269cbf47311fc543d7df8f472ce39d3
slaviktbs <slaviktbs@gmail.com>
2025-02-03 18:06:16 +0300

    small change in bi-elliptic transfer

--- semester-I/space_calculations/bi-elliptic transfer.c
+++ semester-I/space_calculations/bi-elliptic transfer.c
@@ -15,11 +15,12 @@ double m_func_tar (double a_tar)
 
 int main(void)
 {
-    double mu, v_par, a_par, v_tr_per, v_tr_apo, v_tar, a_tar, a_tr, delta_v, delta_v_2;
+    double mu, v_par, a_par, v_tr_per, v_tr_apo, v_tar, a_tar, a_tr_1, delta_v, delta_v_2, f;
 
     mu = 398600441999999.0;
     // a_par = 7000 km; 
-    // a_tar = 42164 km;
+    // a_tar = 105000 km;
+    // f = 2;
 
     printf ("\tEnter the radius of the parking orbit (in km): ");    // The user enters a_par 
     scanf ("%lf", &a_par);                   
@@ -27,8 +28,17 @@ int main(void)
     printf ("\tEnter the radius of the target orbit (in km): ");      // The user enters a_tar
     scanf ("%lf", &a_tar);
 
+    printf ("\tEnter the transfer orbit factor: ");      // The user enters f
+    scanf ("%lf", &f);
+
     a_par = m_func_par(a_par);
     a_tar = m_func_tar(a_tar);
+
+    v_par = sqrt(mu/a_par);
+
+    a_tr_1 = (a_tar*f+a_par)/2;
+
+
     
     
     return 0;
