f249627b39cce1a2f30ee107a706c0b542ed4441
slaviktbs <slaviktbs@gmail.com>
2025-02-10 15:15:33 +0300

    starting calculations

--- semester-I/space_calculations/bi-elliptic transfer.c
+++ semester-I/space_calculations/bi-elliptic transfer.c
@@ -15,7 +15,7 @@ double m_func_tar (double a_tar)
 
 int main(void)
 {
-    double mu, v_par, a_par, v_tr_per, v_tr_apo, v_tar, a_tar, a_tr_1, delta_v, delta_v_2, f;
+    double mu, v_par, a_par, v_tr_per_1, v_tr_per_2, v_tr_apo_1, v_tr_apo_2, v_tar, a_tar, a_tr_1, a__tr_2, delta_v_1, delta_v_2, delta_v_3, f;
 
     mu = 398600441999999.0;
     // a_par = 7000 km; 
@@ -36,10 +36,24 @@ int main(void)
 
     v_par = sqrt(mu/a_par);
 
-    a_tr_1 = (a_tar*f+a_par)/2;
+    a_tr_1 = (a_tar*f + a_par)/2;
 
+    v_tr_per_1 = sqrt(mu*(2/a_par-1/a_tr_1));   // !! ?? ПРОВЕРИТЬ ?? !!
 
+    delta_v_1 = v_tr_per_1 - v_par;
+
+    v_tr_apo_1 = sqrt(mu*(2/a_tar-1/a_tr_1));   // !! ?? ПРОВЕРИТЬ ?? !!    ИЛИ 2/(2*a_tr_1-a_par) ?? !!
+
+    a__tr_2 =  (a_tar - a_par + 2*a_tr_1)/2;  
     
-    
+    v_tr_apo_2 = sqrt(mu*(2/a_tar-1/a__tr_2));   // !! ?? ПРОВЕРИТЬ ?? !!
+
+    delta_v_2 = v_tr_apo_2 - v_tr_apo_1;
+
+    v_tr_per_2 = sqrt(mu*(2/a_par-1/a__tr_2));   // !! ?? ПРОВЕРИТЬ ?? !!
+
+    v_tar = sqrt(mu/a_tar);
+
+    delta_v_3 = v_tr_per_2 - v_tar;
     return 0;
 }
