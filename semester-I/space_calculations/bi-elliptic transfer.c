1d664772acd7749fc4658ee5cdc604d5e5242607
slaviktbs <slaviktbs@gmail.com>
2025-02-17 09:33:55 +0300

    bi-elliptic transfer is DONE!!

--- semester-I/space_calculations/bi-elliptic transfer.c
+++ semester-I/space_calculations/bi-elliptic transfer.c
@@ -1,4 +1,5 @@
-/* The program calculates the orbital inclination change (Problem 2 of the third presentation) with calculations made in the SI system.  */
+/* The program calculates the bi-elliptic transfer (Problem 1 of the fourth presentation) with calculations made in the SI system. */
+
 
 #include <stdio.h>
 #include <math.h>
@@ -15,7 +16,7 @@ double m_func_tar (double a_tar)
 
 int main(void)
 {
-    double mu, v_par, a_par, v_tr_per_1, v_tr_per_2, v_tr_apo_1, v_tr_apo_2, v_tar, a_tar, a_tr_1, a__tr_2, delta_v_1, delta_v_2, delta_v_3, f;
+    double mu, v_par, a_par, v_tr_per_1, v_tr_per_2, v_tr_apo_1, v_tr_apo_2, v_tar, a_tar, a_tr_1, a_tr_2, delta_v_1, delta_v_2, delta_v_3, f;
 
     mu = 398600441999999.0;
     // a_par = 7000 km; 
@@ -38,22 +39,29 @@ int main(void)
 
     a_tr_1 = (a_tar*f + a_par)/2;
 
-    v_tr_per_1 = sqrt(mu*(2/a_par-1/a_tr_1));   // !! ?? ПРОВЕРИТЬ ?? !!
+    v_tr_per_1 = sqrt(mu*(2/a_par-1/a_tr_1));
 
     delta_v_1 = v_tr_per_1 - v_par;
 
-    v_tr_apo_1 = sqrt(mu*(2/a_tar-1/a_tr_1));   // !! ?? ПРОВЕРИТЬ ?? !!    ИЛИ 2/(2*a_tr_1-a_par) ?? !!
+    v_tr_apo_1 = sqrt(mu*(2/(2*a_tr_1-a_par)-1/a_tr_1));
 
-    a__tr_2 =  (a_tar - a_par + 2*a_tr_1)/2;  
+    a_tr_2 =  (a_tar - a_par + 2*a_tr_1)/2;  
     
-    v_tr_apo_2 = sqrt(mu*(2/a_tar-1/a__tr_2));   // !! ?? ПРОВЕРИТЬ ?? !!
+    v_tr_apo_2 = sqrt(mu*(2/(2*a_tr_2-a_tar)-1/a_tr_2));
 
     delta_v_2 = v_tr_apo_2 - v_tr_apo_1;
 
-    v_tr_per_2 = sqrt(mu*(2/a_par-1/a__tr_2));   // !! ?? ПРОВЕРИТЬ ?? !!
+    v_tr_per_2 = sqrt(mu*(2/a_tar-1/a_tr_2));
 
     v_tar = sqrt(mu/a_tar);
 
     delta_v_3 = v_tr_per_2 - v_tar;
+
+    printf ("\tdelta v1 = %lf m/s\n", delta_v_1);
+
+    printf ("\tdelta v2 = %lf m/s\n", delta_v_2);
+
+    printf ("\tdelta v3 = %lf m/s\n", delta_v_3);
+    
     return 0;
 }
