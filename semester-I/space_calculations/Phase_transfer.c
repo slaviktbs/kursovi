77a03b9ea49126e3c85512c51b48de00bf94feb1
slaviktbs <slaviktbs@gmail.com>
2025-02-24 10:30:53 +0300

    Phase transfer is in process

--- semester-I/space_calculations/Phase_transfer.c
+++ semester-I/space_calculations/Phase_transfer.c
@@ -9,25 +9,24 @@ double m_func_orig (double a_orig)
     return a_orig*1000;
 }
 
-double m_func_phi (double pli)
+double m_func_t_ph (double t_ph)
 {
-    return pli*60;
+    return t_ph*60;
 }
-
 
 int main(void)
 {
-    double mu, delta_v_1, delta_v_2, phi, T, a_orig, a_ph, t_ph, T_ph, v_orig, v_ph_apo, v_ph_per, e;
+    double mu, delta_v_1, delta_v_2, T, a_orig, a_ph, t_ph, T_ph, v_orig, v_ph_apo, v_ph_per, e;
     int n, choise;
 
     mu = 398600441999999.0;
     e = 0.00001;
     // a_par = 42164 km;
 
-    a_orig = m_func_orig(a_orig);
-
     printf ("\tEnter the radius of the parking orbit (in km): ");    // The user enters a_par 
     scanf ("%lf", &a_orig);
+
+    a_orig = m_func_orig(a_orig);
 
     printf ("\tNumber of periods in phase orbit before returning to the initial orbit: ");    // The user enters n
     scanf ("%d", &n);
@@ -35,7 +34,7 @@ int main(void)
     printf ("\tPhase difference of the two satellites (in minutes): ");   // The phase transfer 
     scanf ("%lf", &t_ph);
 
-    phi = m_func_phi(phi);   
+    t_ph = m_func_t_ph(t_ph);   
 
     printf("\tDepending on whether the goal should be reached or waited for, enter the relevant data: \n");
     printf("\t\t1 - for the goal to be reached \n");
@@ -54,7 +53,7 @@ int main(void)
 
     T = 2*M_PI*sqrt(pow(a_orig,3)/mu);    // The period of the parking orbits
 
-    T_ph = T+t_ph/n;    // The period of the phase orbit
+    T_ph = T + t_ph/n;    // The period of the phase orbit
 
     a_ph = pow(mu*pow(T_ph/(2*M_PI),2),1.0/3);    // The radius of the phase orbit
 
@@ -75,7 +74,6 @@ int main(void)
         delta_v_1 = v_ph_per - v_orig;
 
         delta_v_2 = (-1.0)*delta_v_1;   //НЕ работает как должно, в зависимости от waiting или reached должны быть дельты противоположны по знаку !!!
-        
     }
 
     printf ("\tdelta v1 = %lf m/s\n", delta_v_1);
